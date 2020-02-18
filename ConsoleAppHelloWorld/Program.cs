d2bf105b4eec7a13fb23d750419d4dff2f3bf900
SilinAlex18 <41696048+SilinAlex18@users.noreply.github.com>
2020-02-18 11:52:46 +0200

    Hello, World

--- ConsoleAppHelloWorld/Program.cs
+++ ConsoleAppHelloWorld/Program.cs
@@ -10,6 +10,8 @@ namespace ConsoleAppHelloWorld
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
         }
     }
 }
